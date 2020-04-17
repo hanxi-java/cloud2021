fff1b649d0b6e6f2f82176ed5a529b32aab603fd
hanxi <634498162@qq.com>
2020-04-17 20:41:49 +0800

    对自己写的cloud教程项目第二次提交

--- cloud-api-commons/src/main/java/com/atguigu/cloud/entities/TTTT.java
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/entities/TTTT.java
@@ -1,8 +1,9 @@
 package com.atguigu.cloud.entities;
 
 public class TTTT {
-  System.out.println("今天");
+
     public static void main(String[] args) {
+        System.out.println("今天");
         System.out.println("明天");
     }
 }
